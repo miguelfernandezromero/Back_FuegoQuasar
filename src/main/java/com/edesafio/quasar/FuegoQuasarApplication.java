b6d2967fb3bfea7e27ff4696bbc3ca102f54f0a8
miguelfernandezromero <miguelfernandezromero@gmail.com>
2022-03-05 20:01:30 -0500

    Miguel
    Initial commit

---
+++ src/main/java/com/edesafio/quasar/FuegoQuasarApplication.java
@@ -0,0 +1,14 @@
+package com.edesafio.quasar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuegoQuasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuegoQuasarApplication.class, args);
+		System.out.println("Hola Mundo");
+	}
+
+}
---
+++ src/test/java/com/edesafio/quasar/FuegoQuasarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edesafio.quasar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuegoQuasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
